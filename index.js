eb3f05d01f7950f6bc573f42cf1a3d2b3f023416
Justin Evans <evansjustindavid@gmail.com>
2020-01-29 18:25:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function shout(string) {
+return string.toUpperCase()
+  
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+  
+}
+
+function logShout(string) {
+  console.log (`${string}`.toUpperCase())
+
+}
+
+function logWhisper(string) {
+  console.log(`${string}`.toLowerCase())
+  
+}
+
+  
+  
+function sayHiToGrandma(string) {
+  
+  var uppercase = "HELLO!"
+  
+  uppercase.toUpperCase(string) === 
+  uppercase
+  
+  var lowercase = 'hello!'
+  
+  lowercase.toLowerCase(string) ===
+  lowercase
+  
+  if ("hello!") {
+    return "I can't hear you!"
+  } else if ("HELLO!") {
+    return "YES INDEED!"
+  } else if ("I love you, Grandma.") {
+  return "I love you, too."
+}
+}
